de620014f76a84595c27202ebd45bfa816218f84
Abdelkrim BELLAGNECH <abdelkrimbellagnech99@gmail.com>
2024-03-16 15:46:03 +0000

    Implemeting the Routing + Adding Link & LinkContainers

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,6 +8,8 @@ import {
   NavbarToggle,
 } from "react-bootstrap";
 import { FaShoppingCart, FaUser } from "react-icons/fa";
+// because we can't just replace Navbar.Brand's href, that why we use LinkContainer
+import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/logo.png";
 
 const Header = () => {
@@ -15,24 +17,31 @@ const Header = () => {
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          {/*So that the elements of the navbar don't stretch all the way to the edges */}
-          <NavbarBrand href="/">
-            <img src={logo} alt="K-Cart" />
-            K-Cart
-          </NavbarBrand>
+          {/*Container : So that the elements of the navbar don't stretch all the way to the edges */}
+          <LinkContainer to="/">
+            <NavbarBrand>
+              <img src={logo} alt="K-Cart" />
+              K-Cart
+            </NavbarBrand>
+          </LinkContainer>
+
           <NavbarToggle aria-controls="basic-navbar-nav"></NavbarToggle>
           <NavbarCollapse id="basic-navbar-nav">
             {/* It is in the collapse where we put the actual Nav */}
             <Nav className="ms-auto">
               {/* ms-auto : this is gonna move the nav links to the right , not next to the logo */}
               {/* Link to the Cart  */}
-              <Nav.Link href="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
               {/* Link to the login */}
-              <Nav.Link href="/login">
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </NavbarCollapse>
         </Container>
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
-      <a href={`/product/${product._id}`}>
+      {/* We are using Link instead of 'a' elements in order to use the benefit of the routers (same with href and to) */}
+      <Link to={`/product/${product._id}`}>
         {/* The variant="top" setting indicates that the image will be displayed at the top of the card.  */}
         <Card.Img src={product.image} variant="top"></Card.Img>
-      </a>
+      </Link>
       <Card.Body>
-        <a href={`/product/${product._id}`}>
+        <Link to={`/product/${product._id}`}>
           <Card.Title as="div">
             <strong>{product.name}</strong>
           </Card.Title>
-        </a>
+        </Link>
         <Card.Text as="h3">${product.price}</Card.Text>
       </Card.Body>
     </Card>
