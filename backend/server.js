4e09c2b3b4aa409a760ce3a90437cdf9fea98e03
Abdelkrim7Be <abdelkrimbellagnech99@gmail.com>
2024-06-12 19:20:51 +0100

    User Routes & Controller Added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ dotenv.config()
 import {notFound, errorHandeler} from "./middleware/errorMiddleware.js"
 import connectDB from './config/db.js'
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 // The frontend is working on the port : 300, so the backend is on the port 5000
 const port = process.env.PORT || 5000;
@@ -27,6 +28,7 @@ const app = express();
 // });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandeler);
